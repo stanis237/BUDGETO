f613895eb23cce6747e7aac97c58cf74a177b47e
stanis miafo <stanismiafo348@gmail.com>
2026-09-04 09:40:26 +0100

    third  deploy friday

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,23 +6,23 @@ plugins {
 
 android {
     namespace = "com.budgeto.budgeto"
-
-    compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    compileSdk = 35 // Stable version for Android 15
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
+
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         applicationId = "com.budgeto.budgeto"
+        // Use flutter extension values but allow fallback
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
